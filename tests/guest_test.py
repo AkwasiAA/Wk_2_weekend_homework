ea4c367ff8596bb3a2724ed7e0d1d1961c6020d0
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-06-27 18:04:47 +0100

    fifth commit

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,20 +1,21 @@
 import unittest
 from classes.guest import *
+from classes.song import *
 
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
-        self.guest = Guest("Lydia", "Room 1")
+        self.guest = Guest("Lydia", "Room 1", self.promiscuous)
+        self.promiscuous = Song("Promiscuous", "Nelly Furtado", "Pop")
 
     def test_guest_has_name(self):
-        pass
+        self.assertEqual("Lydia", self.guest.name)
 
     def test_guest_has_booking(self):
-        pass
+        self.assertEqual("Room 1", self.guest.room_number_booked)
 
-    def test_guest_check_in(self):
-        pass
+    def test_guest_has_favourite_song(self):
+        self.assertEqual(self.promiscuous, self.guest.favourite_song)
 
-    def test_guest_check_out(self):
+    def test_fav_song_is_on_playlist(self):
         pass
-
